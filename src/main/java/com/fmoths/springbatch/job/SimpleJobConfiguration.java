1502963b909dbbcd0aa986c833faa03cf0bfd9af
kdpark1623 <kdpark1623@wemakeprice.com>
2020-08-05 15:05:44 +0900

    JobParameter reqDate 추가

--- src/main/java/com/fmoths/springbatch/job/SimpleJobConfiguration.java
+++ src/main/java/com/fmoths/springbatch/job/SimpleJobConfiguration.java
@@ -5,13 +5,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@Slf4j
-@RequiredArgsConstructor
+@Slf4j //log 사용을 위한 어노테이션
+@RequiredArgsConstructor // 생성자 DI를 위한 lombok 어노테이션
 @Configuration
 public class SimpleJobConfiguration {
     private final JobBuilderFactory jobBuilderFactory;
@@ -21,17 +23,19 @@ public class SimpleJobConfiguration {
     public Job simpleJob(){
         //simpleJob 이라는 배치 생성
         return jobBuilderFactory.get("simpleJob")
-                .start(simpleStep1())
+                .start(simpleStep1(null))
                 .build();
     }
 
     @Bean
-    public Step simpleStep1(){
+    @JobScope
+    public Step simpleStep1 (@Value("#{JobParameters[requestDate]}") String requestDate){
         //simpleStep1 이라는 배치의 스텝 생성
         return stepBuilderFactory.get("simpleStep1")
                 //step 안에서 수행될 step을 커스텀한다.
                 .tasklet((contribution, chunkContext) -> {
                     log.info(">>>>> This is Step1");
+                    log.info(">>>>> requestDate = {}", requestDate);
                     return RepeatStatus.FINISHED;
                 })
                 .build();
